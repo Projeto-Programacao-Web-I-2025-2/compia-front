66ef0e1208a7742bdfedac6efe635c8376c942e6
KaioCar64 <kaio.silva.carvalho@ccc.ufcg.edu.br>
2026-03-09 23:49:01 -0300

    feat: endereço usuário

--- src/services/enderecoService.js
+++ src/services/enderecoService.js
@@ -1,3 +1,4 @@
+import api from "./api";
 import { message } from "antd";
 import axios from "axios";
 
@@ -13,5 +14,52 @@ export const buscaCep = async (cep) => {
     } catch (err) {
         console.log("Erro ao buscar CEP:", err);
         return null;
+    }
+}
+
+export const enderecoUser = async () => {
+    const user = await api.get("/auth/users/me/");
+
+    try {
+        if (user.data.role === "CLIENTE") {
+            const response = await api.get('/vendedores/me/');
+            return response.data.endereco;
+        } 
+
+        const response = await api.get('/vendedores/me/');
+        return response.data.endereco;
+    } catch (err) {
+        console.log("Erro ao carregar endereço:", err);
+        return;
+    }
+}
+
+export const cadastrarEndereco = async ({rua, numero, bairro, cidade, estado, complemento, cep}) => {
+    const user = await api.get("/auth/users/me/");
+
+    try {
+        if (user.data.rolle === "CLIENTE") {
+            const response = await api.patch('clientes/me/' , {endereco: {rua, numero, bairro, cidade, estado, complemento, cep}});
+            return response.data;
+        }
+
+        const response = await api.patch('vendedores/me/' , {endereco: {rua, numero, bairro, cidade, estado, complemento, cep}});
+        return response.data;
+        
+    } catch (error) {
+        if (error.response && error.response.data) {
+            const data = error.response.data;
+
+            const mensagens = Object.keys(data).map((campo) => {
+                const erroProprio = data[campo];
+                return `${campo}: ${Array.isArray(erroProprio) ? erroProprio.join("; ") : erroProprio}`;
+            });
+
+            const mensagemFinal = mensagens.join(" | ");
+            
+            message.error(mensagemFinal);
+        }
+
+        throw error
     }
 }
