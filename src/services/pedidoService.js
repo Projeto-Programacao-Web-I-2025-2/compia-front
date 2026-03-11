63854b6414b923bcdc11df11fb2177f5dd465139
KaioCar64 <kaio.silva.carvalho@ccc.ufcg.edu.br>
2026-03-10 22:15:33 -0300

    fix: limpa carrinho

--- src/services/pedidoService.js
+++ src/services/pedidoService.js
@@ -4,10 +4,10 @@ import { useCarrinho } from "../contexts/CarrinhoContext";
 export const criaPedido = async (carrinhosIds) => {
     const body = carrinhosIds.map(([id, qtd]) => ({produto: id, quantidade: qtd}));
 
-    console.log(body);
-
     try {
         const response = await api.post('/pedidos/', {itens: body})
+        localStorage.removeItem('carrinhoIds');
+
         return response.data;
     } catch (err) {
         console.log(err.data);
